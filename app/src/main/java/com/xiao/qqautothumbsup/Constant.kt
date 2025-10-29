5925335033b0e690fe5054e66449da953f466bf7
gh-xiao <2845426491@qq.com>
2025-10-29 14:31:11 +0800

    重构无障碍服务与悬浮窗功能

---
+++ app/src/main/java/com/xiao/qqautothumbsup/Constant.kt
@@ -0,0 +1,9 @@
+package com.xiao.qqautothumbsup
+
+object Constant {
+    const val EXTRA_FRAGMENT_ARG_KEY = ":settings:fragment_args_key"
+    const val EXTRA_SHOW_FRAGMENT_ARGUMENTS = ":settings:show_fragment_args"
+    const val QQ_PACKAGE_NAME = "com.tencent.mobileqq"
+    const val QQ_COMPONENT_NAME = "com.tencent.mobileqq.activity.SplashActivity"
+    const val IS_AUTO_LIKE_ENABLE = "isLike"
+}
--- app/src/main/java/com/xiao/qqautothumbsup/MainAccessibilityService.kt
+++ app/src/main/java/com/xiao/qqautothumbsup/MainAccessibilityService.kt
@@ -1,97 +1,57 @@
-package com.xiao.qqautothumbsup;
+package com.xiao.qqautothumbsup
 
-import android.accessibilityservice.AccessibilityService;
-import android.os.Build;
-import android.util.Log;
-import android.view.accessibility.AccessibilityEvent;
-import android.view.accessibility.AccessibilityManager;
-import android.view.accessibility.AccessibilityNodeInfo;
+import android.accessibilityservice.AccessibilityService
+import android.util.Log
+import android.view.accessibility.AccessibilityEvent
+import android.view.accessibility.AccessibilityNodeInfo
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 
-import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
+class MainAccessibilityService : AccessibilityService() {
+    private val scope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
+    private var lastClickTime = 0L
 
-public class MainAccessibilityService extends AccessibilityService {
-    private static final String TAG = MainAccessibilityService.class.getSimpleName();
-    // 创建一个带定时器固定大小的线程池
-    private static ScheduledExecutorService threadPool = Executors.newSingleThreadScheduledExecutor();
-    private static ScheduledFuture<?> future;
-
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        Log.i(TAG, "MainAccessibilityService onCreate: 无障碍服务已启动");
+    companion object {
+        private val TAG: String = MainAccessibilityService::class.java.simpleName
     }
 
-    @Override
-    protected void onServiceConnected() {
-        super.onServiceConnected();
-
-        future = threadPool.scheduleAtFixedRate(() -> {
-            if (!MainActivity.isLike()) return;
-            try {
-                AccessibilityManager am = (AccessibilityManager) getSystemService(ACCESSIBILITY_SERVICE);
-                if (am != null && am.isEnabled() && MainActivity.isLike()) {
-                    if (getRootInActiveWindow() == null) return;
-                    /* 获取匹配的节点列表 */
-                    List<AccessibilityNodeInfo> nodes = getRootInActiveWindow()
-                            .findAccessibilityNodeInfosByViewId("com.tencent.mobileqq:id/dyw");
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) nodes.stream()
-                            /* 过滤出可见和可点击的节点 */
-                            .filter(node -> node.isVisibleToUser() && node.isClickable())
-                            /* 模拟点击节点 */
-                            .forEach(node -> node.performAction(AccessibilityNodeInfo.ACTION_CLICK));
-                        /* 遍历节点列表 */
-                    else for (AccessibilityNodeInfo node : nodes) {
-                        /* 判断节点是否可见和可点击 */
-                        if (node.isVisibleToUser() && node.isClickable())
-                            /* 模拟点击节点 */
-                            node.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-                    }
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+        try {
+            if (System.currentTimeMillis() - lastClickTime < 100) return
+            lastClickTime = System.currentTimeMillis()
+            /* 检查自动点赞是否已经开启 */
+            val isAutoLikeEnable = SpUtil.instance.get(Constant.IS_AUTO_LIKE_ENABLE, false) == true
+            if (!isAutoLikeEnable) return
+            /* 检查无障碍服务是否正常启动 */
+//            val am = getSystemService(ACCESSIBILITY_SERVICE) as? AccessibilityManager ?: return
+//            if (!am.isEnabled) return
+            /* 检查当前窗口的根节点是否为空 */
+            val root = /*event?.source ?:*/ rootInActiveWindow ?: return
+            /* 获取匹配的节点列表 */
+            val nodes = root
+                .findAccessibilityNodeInfosByViewId("com.tencent.mobileqq:id/dyw")
+                /* 过滤出可见和可点击的节点 */
+                ?.filter { it.isVisibleToUser && it.isClickable }
+                ?: return
+            scope.launch {
+                /* 清除空节点 并 模拟点击节点 */
+                nodes.filterNotNull().forEach { node ->
+                    node.performAction(AccessibilityNodeInfo.ACTION_CLICK)
                 }
-            } catch (Exception e) {
-                Log.e(TAG, "Exception in scheduled task: " + e.getMessage());
             }
-        }, 1, 100, TimeUnit.MILLISECONDS);
-    }
-
-    /**
-     * 触发了需要监听的无障碍事件后回调
-     *
-     * @param event The new event. This event is owned by the caller and cannot be used after
-     *              this method returns. Services wishing to use the event after this method returns should
-     *              make a copy.
-     */
-    @Override
-    public void onAccessibilityEvent(AccessibilityEvent event) {
-    }
-
-    /**
-     * 无障碍服务断开后回调
-     */
-    @Override
-    public void onInterrupt() {
-
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        cancelTask();
-    }
-
-    public static void cancelTask() {
-        // 在服务销毁时取消并释放定时任务和线程池
-        if (future != null) {
-            future.cancel(true);
-            future = null;
+        } catch (e: Exception) {
+            Log.e(TAG, "Exception in scheduled task: " + e.message)
         }
-        if (threadPool != null) {
-            threadPool.shutdown();
-            threadPool = null;
-        }
+    }
+
+    override fun onInterrupt() {
+    }
+
+    override fun onDestroy() {
+        scope.cancel()
+        super.onDestroy()
     }
 }
